5cfe3fef40c8d5132163ef9ec07c1871baf96096
yunjoa <52683681+yunjoa@users.noreply.github.com>
2023-01-05 15:29:37 +0900

    added loading page

--- main.js
+++ main.js
@@ -107,3 +107,31 @@
   });
   activate();
 })();
+
+
+
+$(document).ready(function () {
+  var counter = 0;
+  var c = 0;
+  var i = setInterval(function () {
+    $(".loading-page .counter h1").html(c + "%");
+    $(".loading-page .counter hr").css("width", c + "%");
+
+    
+    // $(".loading-page .counter h1.color").css("width", c + "%");
+    
+    counter++;
+    c++;
+
+    if (counter == 101) {
+      clearInterval(i);
+    }
+  }, 50);
+});
+
+
+$(window).on('load', function() {
+    setTimeout(function(){
+        $(".loading-page").fadeOut();
+    }, 5500);
+});
